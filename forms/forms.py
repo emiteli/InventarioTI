44f1aff7b051b4f825347dda172e1e2b177f8703
Emiteli indústria eletrônica <153731724+emiteli@users.noreply.github.com>
2024-10-08 16:41:16 -0300

    Funcionario

--- forms/forms.py
+++ forms/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, SubmitField, SelectField, FileField
+from wtforms import PasswordField, StringField, SubmitField, SelectField, FileField, IntegerField
 from wtforms.validators import DataRequired, EqualTo, Length, Optional
 
 class LoginForm(FlaskForm):
@@ -34,3 +34,16 @@ class CadastroPatrimonioForm(FlaskForm):
 class UploadFileForm(FlaskForm):
     file = FileField('Upload Excel', validators=[DataRequired()])
     submit = SubmitField('Upload')
+
+class FilterForm(FlaskForm):
+    filial = StringField('Filial', validators=[Optional()])
+    grupo = IntegerField('Grupo', validators=[Optional()])
+    classificacao = StringField('Classificação', validators=[Optional()])
+    codigo_bem = StringField('Código do Bem', validators=[Optional()])
+    nota_fiscal = IntegerField('Nota Fiscal', validators=[Optional()])
+    descricao_sintetica = StringField('Descricao', validators=[Optional()])
+    submit = SubmitField('Filtrar')
+
+class AlterarStatusForm(FlaskForm):
+    novo_status = SelectField('Novo Status', choices=[('Ativo', 'Ativo'), ('Inativo', 'Inativo'),('Ferias', 'Ferias')], default='', validators=[DataRequired()])
+    submit = SubmitField('Alterar Status')
