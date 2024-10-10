934f1d70e8141eededda643590b2e0974fb06d81
Emiteli indústria eletrônica <153731724+emiteli@users.noreply.github.com>
2024-10-10 14:29:25 -0300

    update versao 2.0

--- models/models.py
+++ models/models.py
@@ -6,47 +6,6 @@ from datetime import datetime, timezone
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(150), nullable=False)
-
-    def set_password(self, password):
-        self.password = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password, password)
-
-class Ativo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(150), nullable=False)
-    descricao = db.Column(db.String(300))
-    numero_serie = db.Column(db.String(100), nullable=False, unique=True)
-    localizacao = db.Column(db.String(100), nullable=False)
-    filial = db.Column(db.String(50), nullable=True)
-    cod_base_bem = db.Column(db.String(50), nullable=True)
-    codigo_item = db.Column(db.String(50), nullable=True)
-    tipo_ativo = db.Column(db.String(50), nullable=True)
-    historico = db.Column(db.String(300), nullable=True)
-    conta = db.Column(db.String(50), nullable=True)
-    tipo_deprec = db.Column(db.String(50), nullable=True)
-    ativo_origem = db.Column(db.String(100), nullable=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    patrimonio_id = db.Column(db.Integer, db.ForeignKey('patrimonios.id'), nullable=True)
-    patrimonio = db.relationship('Patrimonio', backref='ativo')
-
-class Patrimonio(db.Model):
-    __tablename__ = 'patrimonios'
-    
-    id = db.Column(db.Integer, primary_key=True)
-    placa_patrimonio = db.Column(db.String(50), nullable=False, unique=True)
-    codigo_compra = db.Column(db.String(50), nullable=False)
-    cod_nfe = db.Column(db.String(50), nullable=False)
-    usuario = db.Column(db.String(150), nullable=False)
-    fabricante = db.Column(db.String(100), nullable=False)
-    processador = db.Column(db.String(100), nullable=False)
-    modelo = db.Column(db.String(100), nullable=False)
-    data_cadastro = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
-    
-    def __repr__(self):
-        return f'<Patrimonio {self.placa_patrimonio}>'
     
 class Asset(db.Model):
     id = db.Column(db.Integer, primary_key=True)
--- routes/routes.py
+++ routes/routes.py
@@ -1,14 +1,12 @@
-from flask import Blueprint, render_template, redirect, url_for, flash, request, current_app, send_file
+from flask import Blueprint, render_template, redirect, url_for, flash, request, current_app, send_file 
 from flask_login import login_user, logout_user, login_required, current_user
 from extensions import db, login_manager 
-from models.models import User, Ativo, Patrimonio, Asset, Funcionario
-from forms.forms import LoginForm, CadastroAtivoForm, RegisterForm, CadastroPatrimonioForm, UploadFileForm,  FilterForm, AlterarStatusForm
-from flask_wtf.csrf import generate_csrf
-from sqlalchemy.exc import IntegrityError
-import io
-import os
+from models.models import User,  Asset, Funcionario
+from forms.forms import LoginForm, UploadFileForm,  FilterForm, AlterarStatusForm
+import io, os
 import pandas as pd
 from werkzeug.utils import secure_filename
+from ldap3 import Server, Connection, ALL, NTLM
 
 routes = Blueprint('routes', __name__)
 
@@ -22,33 +20,40 @@ def home():
         return redirect(url_for('routes.listar_ativos'))
     return redirect(url_for('routes.login'))
 
-@routes.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user:
-            flash('Nome de usuário já está em uso!', 'danger')
-            return redirect(url_for('routes.register'))
-        
-        new_user = User(username=form.username.data)
-        new_user.set_password(form.password.data)
-        db.session.add(new_user)
-        db.session.commit()
-        flash('Cadastro realizado com sucesso. Agora você pode fazer login!', 'success')
-        return redirect(url_for('routes.login'))
-    return render_template('register.html', form=form)
-
 @routes.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user and user.check_password(form.password.data):  
-            login_user(user)
-            return redirect(url_for('routes.listar_ativos'))
+        username = form.username.data
+        password = form.password.data
+
+        ldap_host = current_app.config['LDAP_HOST']
+        if not ldap_host:
+            flash('O servidor LDAP não está configurado.', 'danger')
+            return redirect(url_for('routes.login'))
+
+        server = Server(ldap_host, get_info=ALL)
+        DOMAIN = 'emiteli.com.br'
+        user_with_domain = f"{DOMAIN}\\{username}"
+
+        conn = Connection(server, user=user_with_domain, password=password, authentication=NTLM)
+
+        if conn.bind():  # Tentativa de autenticar via LDAP
+            # Aqui você pode buscar ou criar o usuário no banco de dados, se necessário
+            user = User.query.filter_by(username=username).first()  # Verificando se o usuário existe no banco de dados
+            if not user:
+                # Se o usuário não existir no banco de dados, você pode criar um novo registro
+                user = User(username=username)
+                db.session.add(user)  # Adiciona o novo usuário à sessão
+                db.session.commit()  # Salva as alterações
+
+            login_user(user)  # Logando o usuário
+            flash('Login bem-sucedido!', 'success')
+            return redirect(url_for('routes.listar_ativos'))  # Redireciona para a lista de ativos
         else:
-            flash('Login inválido!')
+            flash('Falha na autenticação. Verifique suas credenciais.', 'danger')
+
     return render_template('login.html', form=form)
 
 @routes.route('/logout')
@@ -56,32 +61,6 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('routes.login'))
-
-@routes.route('/cadastro_ativos', methods=['GET', 'POST'])
-@login_required
-def cadastro_ativos():
-    form = CadastroAtivoForm()
-    form.patrimonio_id.choices = [(p.id, p.placa_patrimonio) for p in Patrimonio.query.all()]
-    if form.validate_on_submit():
-        novo_ativo = Ativo(
-            nome=form.nome.data,
-            descricao=form.descricao.data,
-            numero_serie=form.numero_serie.data,
-            localizacao=form.localizacao.data,
-            user_id=current_user.id,
-            patrimonio_id=form.patrimonio_id.data
-        )
-        
-        try:
-            db.session.add(novo_ativo)
-            db.session.commit()
-            flash('Ativo cadastrado com sucesso!')
-            return redirect(url_for('routes.listar_ativos'))
-        except IntegrityError:
-            db.session.rollback()
-            flash('Erro: O número de série deve ser único. Já existe um ativo cadastrado com esse número de série.', 'danger')
-
-    return render_template('cadastro_ativos.html', form=form)
 
 @routes.route('/listar_ativos', methods=['GET', 'POST'])
 @login_required
@@ -110,101 +89,39 @@ def listar_ativos():
     ativos = query.all()
     return render_template('listar_ativos.html', form=form, ativos=ativos)
 
-
-
-@routes.route('/cadastrar_patrimonio', methods=['GET', 'POST'])
-@login_required  
-def cadastrar_patrimonio():
-    form = CadastroPatrimonioForm()
-    if form.validate_on_submit():
-        patrimonio = Patrimonio(
-            placa_patrimonio=form.placa_patrimonio.data,
-            codigo_compra=form.codigo_compra.data,
-            cod_nfe=form.cod_nfe.data,
-            usuario=form.usuario.data,
-            fabricante=form.fabricante.data,
-            processador=form.processador.data,
-            modelo=form.modelo.data
-        )
-        db.session.add(patrimonio)
-        db.session.commit()
-        flash('Patrimônio cadastrado com sucesso!', 'success')
-        return redirect(url_for('routes.listar_patrimonios'))
-    return render_template('cadastrar_patrimonio.html', form=form)
-
-@routes.route('/listar_patrimonios')
-@login_required  
-def listar_patrimonios():
-    patrimonios = Patrimonio.query.all()
-    return render_template('listar_patrimonios.html', patrimonios=patrimonios)
-
-@routes.route('/editar_ativo/<int:id>', methods=['GET', 'POST'])
+@routes.route('/upload_and_process', methods=['GET', 'POST'])
 @login_required
-def editar_ativo(id):
-    ativo = Ativo.query.get_or_404(id)
-    form = CadastroAtivoForm(obj=ativo)
-
-    if form.validate_on_submit():
-        ativo.nome = form.nome.data
-        ativo.descricao = form.descricao.data
-        ativo.numero_serie = form.numero_serie.data
-        ativo.localizacao = form.localizacao.data
-        db.session.commit()
-        flash('Ativo atualizado com sucesso!')
-        return redirect(url_for('routes.listar_ativos'))
-
-    return render_template('editar_ativo.html', form=form, ativo=ativo)
-
-@routes.route('/excluir_ativo/<int:id>', methods=['POST'])
-@login_required
-def excluir_ativo(id):
-    ativo = Ativo.query.get_or_404(id)
-    db.session.delete(ativo)
-    db.session.commit()
-    flash('Ativo excluído com sucesso!')
-    return redirect(url_for('routes.listar_ativos'))
-
-@routes.route('/upload_excel', methods=['GET', 'POST'])
-@login_required
-def upload_excel():
+def upload_and_process():
     form = UploadFileForm()
-    
+    planilhas_disponiveis = os.listdir(current_app.config['UPLOAD_FOLDER']) 
+    df_filtered = None
+
     if form.validate_on_submit():
+        
         file = form.file.data
         filename = secure_filename(file.filename)
         file_path = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
         
         file.save(file_path)
-
         flash('Arquivo Excel carregado com sucesso!', 'success')
-    return render_template('upload_excel.html', form=form)
 
-@routes.route('/exibir_dados', methods=['GET', 'POST'])
-@login_required
-def exibir_dados():
-    planilhas_disponiveis = os.listdir(current_app.config['UPLOAD_FOLDER'])  
-    df_filtered = None
-
-    if request.method == 'POST':
-        selected_file = request.form['planilha']
+    if request.method == 'POST' and 'tipo_banco' in request.form:
+        
+        selected_file = form.file.data.filename if form.file.data else request.form['planilha']
         file_path = os.path.join(current_app.config['UPLOAD_FOLDER'], selected_file)
         tipo_banco = request.form['tipo_banco']
 
         if not os.path.exists(file_path):
             flash(f'O arquivo {selected_file} não foi encontrado.', 'danger')
-            return render_template('exibir_dados.html', planilhas_disponiveis=planilhas_disponiveis, df_filtered=df_filtered)
+            return render_template('upload_and_process.html', form=form, planilhas_disponiveis=planilhas_disponiveis, df_filtered=df_filtered)
 
         try:
-            # Lê a planilha, mantendo todos os dados
-            df_cleaned = pd.read_excel(file_path, header=0)  
-
-            # Remover apenas colunas completamente vazias
+            
+            df_cleaned = pd.read_excel(file_path, header=0)
             df_cleaned = df_cleaned.dropna(how='all', axis=1)
-
-            # Preencher valores NaN com None para tratamento posterior no banco de dados
             df_cleaned = df_cleaned.where(df_cleaned.notnull(), None)
 
-            # Define as colunas que você deseja manter
+            
             if tipo_banco == 'asset':
                 df_filtered = df_cleaned.iloc[:, [0, 1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12]]
                 df_filtered.columns = ['Filial', 'Grupo', 'Classificac.', 'Cod. do Bem', 'Item', 'Dt. Aquisição', 'Quantidade', 'Descr. Sint.', 'Num. Placa', 'Cod. Fornec.', 'Loja Fornec.', 'Nota Fiscal']
@@ -234,10 +151,8 @@ def exibir_dados():
                 df_filtered.columns = ['STATUS', 'DEPARTAMENTO', 'NOME', 'LICENCAS', 'CARGO', 'EMAIL']
 
                 for index, row in df_filtered.iterrows():
-                    # Verifique se as colunas obrigatórias não estão vazias
                     if not row['DEPARTAMENTO'] and not row['NOME'] and not row['EMAIL']:
-                        # Se todos os campos estiverem vazios, continue para a próxima linha
-                        continue  # Ignore essa linha e passe para a próxima
+                        continue  
 
                     existing_funcionario = Funcionario.query.filter_by(email=row['EMAIL']).first()
 
@@ -256,29 +171,43 @@ def exibir_dados():
             flash('Dados foram inseridos com sucesso no banco de dados.', 'success')
 
         except Exception as e:
-            db.session.rollback()  # Faça rollback em caso de erro
+            db.session.rollback()
             flash(f'Erro ao processar o arquivo: {str(e)}', 'danger')
 
-    return render_template('exibir_dados.html', planilhas_disponiveis=planilhas_disponiveis, df_filtered=df_filtered)
+    return render_template('upload_and_process.html', form=form, planilhas_disponiveis=planilhas_disponiveis, df_filtered=df_filtered)
+
 
 @routes.route('/listar_funcionarios', methods=['GET', 'POST'])
 @login_required
 def listar_funcionarios():
-    form = AlterarStatusForm()  # Instancia o formulário
+    form = AlterarStatusForm()
+    nome = request.args.get('nome', '')  
+
+    if nome:
+        funcionarios = Funcionario.query.filter(Funcionario.nome.ilike(f'%{nome}%')).all()
+    else:
+        funcionarios = Funcionario.query.all()
+
     
-    if request.method == 'POST' and form.validate_on_submit():
-        funcionario_id = request.form.get('funcionario_id')
-        novo_status = form.novo_status.data
+    if form.validate_on_submit() and request.method == 'POST':
+        funcionario_id = request.form.get('funcionario_id')  
         funcionario = Funcionario.query.get(funcionario_id)
 
         if funcionario:
+            novo_status = form.novo_status.data
             funcionario.status = novo_status
-            db.session.commit()
-            flash('Status alterado com sucesso!', 'success')
+
+            try:
+                db.session.commit()
+                flash('Status alterado com sucesso!', 'success')
+            except Exception as e:
+                db.session.rollback()
+                flash(f'Erro ao salvar no banco: {str(e)}', 'danger')
         else:
             flash('Funcionário não encontrado.', 'danger')
 
-    funcionarios = Funcionario.query.all()  # Recupera todos os funcionários
+        return redirect(url_for('routes.listar_funcionarios', nome=nome))  
+
     return render_template('listar_funcionarios.html', funcionarios=funcionarios, form=form)
 
 @routes.route('/alterar_status/<int:funcionario_id>', methods=['POST'])
@@ -286,29 +215,28 @@ def listar_funcionarios():
 def alterar_status(funcionario_id):
     form = AlterarStatusForm()
 
-    if form.validate_on_submit():  # Valida o formulário
-        novo_status = form.novo_status.data  # Pega o novo status do formulário
-        funcionario = Funcionario.query.get(funcionario_id)  # Busca o funcionário pelo ID
+    if form.validate_on_submit():  
+        novo_status = form.novo_status.data  
+        funcionario = Funcionario.query.get(funcionario_id)  
 
         if funcionario:
-            funcionario.status = novo_status  # Altera o status
+            funcionario.status = novo_status  
             try:
-                db.session.commit()  # Salva no banco de dados
+                db.session.commit()  
                 flash('Status alterado com sucesso!', 'success')
             except Exception as e:
-                db.session.rollback()  # Rollback em caso de erro
+                db.session.rollback()  
                 flash(f'Erro ao salvar no banco: {str(e)}', 'danger')
         else:
             flash('Funcionário não encontrado.', 'danger')
 
-    return redirect(url_for('routes.listar_funcionarios'))  # Redireciona para a lista de funcionários
+    return redirect(url_for('routes.listar_funcionarios'))  
 
 @routes.route('/exportar_funcionarios', methods=['GET'])
 @login_required
 def exportar_funcionarios():
-    funcionarios = Funcionario.query.all()  # Recupera todos os funcionários do banco de dados
+    funcionarios = Funcionario.query.all()  
 
-    # Converte os dados para um DataFrame do pandas
     data = {
         'Status': [funcionario.status for funcionario in funcionarios],
         'Departamento': [funcionario.departamento for funcionario in funcionarios],
@@ -319,15 +247,12 @@ def exportar_funcionarios():
     }
     df = pd.DataFrame(data)
 
-    # Salva o DataFrame em um objeto BytesIO (em memória)
     output = io.BytesIO()
     
-    # Escreve o DataFrame em um arquivo Excel dentro do objeto BytesIO
     with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
         df.to_excel(writer, index=False, sheet_name='Funcionarios')
     
-    output.seek(0)  # Move o ponteiro para o início do arquivo em memória
+    output.seek(0)  
 
-    # Envia o arquivo Excel para download
     return send_file(output, mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 
                      as_attachment=True, download_name='funcionarios_exportados.xlsx')
