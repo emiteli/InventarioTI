5e5d9778733693b4b4c35829b76c3746649aed0a
Emiteli indústria eletrônica <153731724+emiteli@users.noreply.github.com>
2024-10-10 14:28:58 -0300

    update versao 2.0

--- app.py
+++ app.py
@@ -4,15 +4,13 @@ from extensions import db, login_manager, migrate
 app = Flask(__name__)
 app.config.from_object('config.Config')
 
-
 db.init_app(app)
 login_manager.init_app(app)
-login_manager.login_view = 'login'
-migrate.init_app(app, db) 
-
+login_manager.login_view = 'login'  # Alterado aqui
+migrate.init_app(app, db)
 
 from routes.routes import routes
 app.register_blueprint(routes)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000,debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- config.py
+++ config.py
@@ -3,7 +3,14 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = 'Emiteli@123'
+    SECRET_KEY = 'Emiteli@123'  # Chave secreta para segurança
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'site.db') 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(basedir, 'uploads')
+    DEBUG = True
+    
+    # Configurações do LDAP
+    LDAP_HOST = '10.0.21.1'  # Endereço do servidor LDAP
+    LDAP_BASE_DN = 'dc=emiteli,dc=com,dc=br'  # Corrigido: use vírgulas para separar os componentes
+    LDAP_USER_DN = 'ou=users'
+    LDAP_USER_RDN = 'uid'
